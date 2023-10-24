8c08bb6dd07f7c7ab7de1ab0ae0f37817de86681
sachinkumarsain <sachinsain21082004@gmail.com>
2023-10-24 12:26:12 +0530

    connection file work and connection mongodb in severin index file

--- Index.js
+++ Index.js
@@ -1,13 +1,16 @@
 import express from "express"
 import cors from "cors"
 
+import connection from "./Connection";
+
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({origin:"https://localhost:3000"}))
 
-
-app.listen(8080,(req , res)=>{
-    console.log("server started 8080")
+connection.then(() => {
+    app.listen(8080, () => {
+        console.log("server started at port 8080");
+    });
 })
