9a82e84d1c4a445248eb6586ce7e1d45a29ebe46
sachinkumarsain <sachinsain21082004@gmail.com>
2023-10-28 22:43:20 +0530

    candidate schema in aading information

--- Modals/Condedate.js
+++ Modals/Condedate.js
@@ -1,45 +1,109 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
+
+    fullname: {
+        type: String,
+        require: true
+    },
+    profilePic: {
+        type: String,
+        require: true
+    }
+    , course: {
+        type: String,
+        require: true
+    }, email: {
+        type: String,
+        require: true,
+        unique: true,
+    }, address: {
+        type: String,
+        require: true
+    }, mobile: {
+        type: Number,
+        require: true
+    }, linkedin: {
+        type: String,
+        require: true
+    }, github: {
+        type: String,
+        require: true
+    }, summary: {
+        type: String,
+        require: true
+    },
+    Graduation: {
+        startYear: {
+            type:String,
+            required:true
+        },
+        lastYear: {
+            type:String,
+            required:true
+        },
+        location: {
+            type:String,
+            required:true
+        },
+        Degree: {
+            type:String,
+            required:true
+        },
+        university: {
+            type:String,
+            required:true
+        },
+        decripation: {
+            type:String,
+            required:true
+        },
+        skills: {
+            type:String,
+            required:true
+        }
+    }
+    ,
     
-fullname :{
-    type:String,
-    require:true
-},
-profilePic:{
-    type:String,
-    require:true
-}
-,course :{
-    type:String,
-    require:true
-},email :{
-    type:String,
-    require:true,
-    unique:true,
-},address :{
-    type:String,
-    require:true
-},mobile :{
-    type:Number,
-    require:true
-},linkedin :{
-    type:String,
-    require:true
-},github :{
-    type:String,
-    require:true
-},summary :{
-    type:String,
-    require:true
-},skills :{
-    type:String,
-    require:true
-},
-lanuages :{
-    type:String,
-    require:true
-}
+    Intership: {
+        startTime: {
+            type:String,
+            required:true
+        },
+        completeTime: {
+            type:String,
+            required:true
+        },
+        location: {
+            type:String,
+            required:true
+        },
+        Degree: {
+            type:String,
+            required:true
+        },
+        institute: {
+            type:String,
+            required:true
+        },
+        decripation: {
+            type:String,
+            required:true
+        },
+        skills: {
+            type:String,
+            required:true
+        }
+    }
+    ,
+     skills: {
+        type: String,
+        require: true
+    },
+    lanuages: {
+        type: String,
+        require: true
+    }
 
 })
 
